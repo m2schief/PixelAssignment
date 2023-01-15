e34ccc91f48321cab83456a993e5f48f3509ce09
m2schief <mhvs127@gmail.com>
2023-01-15 14:45:32 -0500

    Hungarian Algorithm Verified in PixelVerification

--- PixelVerification.py
+++ PixelVerification.py
@@ -3,15 +3,10 @@
 #Compare image accuracy between algorithms
 
 import collections
-from PIL import Image
+from PIL import Image, ImageCms
 
 def CheckSame(im1: Image.Image, im2: Image.Image):
     im1_data = list(im1.getdata())
     im2_data = list(im2.getdata())
 
     return(collections.Counter(im1_data) == collections.Counter(im2_data))
-
-random50 = Image.open("./images/50random50.bmp")
-random_spiral = Image.open("./generated_images/random_spiral.bmp")
-
-print(CheckSame(random50, random_spiral))
